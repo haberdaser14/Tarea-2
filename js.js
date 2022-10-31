a295fc8f91d95b23a7914dc1555ebd2194448467
Manu <116102630+haberdaser14@users.noreply.github.com>
2022-10-31 12:09:37 -0300

    Se agrega funcionalidad al botón.
    Se le agrega funcionalidad al botón para que se pueda interactuar con el.

--- js.js
+++ js.js
@@ -4,3 +4,24 @@
 // vas a crear un botón de acción que una vez que lo apretás, va a
 // mostrar toda la información junta en un campo de texto
 // Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
+
+// const button = document.querySelector("#ingresar");
+
+
+//     let h1 = document.querySelector("h1")
+//     h1.innerText = `¡Bienvenido ${nombreUsuario}!`
+const mostrarDatosDelUsuario = function(){
+    const nombreUsuario = document.querySelector("#nombre_usuario").value;
+    const segundoNombreUsuario = document.querySelector("#segundo_nombre_usuario").value;
+    const apellidoUsuario = document.querySelector("#apellido_usuario").value;
+    const edadUsuario = document.querySelector("#edad_usuario")
+    return (`${nombreUsuario} ${segundoNombreUsuario} ${apellidoUsuario} ${edadUsuario}`)
+}
+
+const botonMostrarDatos = document.querySelector("#ingresar");
+botonMostrarDatos.onclick = function(){
+    document.querySelector('#mostrar_datos_usuario').value = mostrarDatosDelUsuario();
+    return false;
+}
+
+
